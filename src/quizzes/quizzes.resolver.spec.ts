ca6ed436ce3f645249d754783824515a8bd13929
zonoise <zonoise@gmail.com>
2021-07-12 13:04:11 +0900

    test環境

--- src/quizzes/quizzes.resolver.spec.ts
+++ src/quizzes/quizzes.resolver.spec.ts
@@ -1,6 +1,10 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { QuizzesModule } from './quizzes.module';
 import { QuizzesResolver } from './quizzes.resolver';
 import { QuizzesService } from './quizzes.service';
+import { QuizSchema, Quiz } from './schema/quiz.schema';
 
 describe('QuizzesResolver', () => {
   let resolver: QuizzesResolver;
@@ -8,9 +12,22 @@ describe('QuizzesResolver', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [QuizzesResolver, QuizzesService],
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forFeature([{ name: Quiz.name, schema: QuizSchema }]),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGODB_URI'),
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          }),
+          inject: [ConfigService],
+        }),
+      ],
     }).compile();
 
-    resolver = module.get<QuizzesResolver>(QuizzesResolver);
+    resolver = await module.resolve<QuizzesResolver>(QuizzesResolver);
   });
 
   it('should be defined', () => {
--- src/quizzes/quizzes.service.spec.ts
+++ src/quizzes/quizzes.service.spec.ts
@@ -1,15 +1,31 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuizzesService } from './quizzes.service';
+import { Quiz, QuizSchema } from './schema/quiz.schema';
 
 describe('QuizzesService', () => {
   let service: QuizzesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forFeature([{ name: Quiz.name, schema: QuizSchema }]),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGODB_URI'),
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          }),
+          inject: [ConfigService],
+        }),
+      ],
       providers: [QuizzesService],
     }).compile();
 
-    service = module.get<QuizzesService>(QuizzesService);
+    service = await module.resolve<QuizzesService>(QuizzesService);
   });
 
   it('should be defined', () => {

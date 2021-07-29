fd56ad6e13b9700b8e37fe5d76d79c7a782ccd6c
zonoise <zonoise@gmail.com>
2021-07-29 16:03:20 +0900

    fix 色々

--- src/quizzes/quizzes.resolver.ts
+++ src/quizzes/quizzes.resolver.ts
@@ -2,7 +2,6 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { QuizzesService } from './quizzes.service';
 import { Quiz } from './entities/quiz.entity';
 import { CreateQuizInput } from './dto/create-quiz.input';
-import { UpdateQuizInput } from './dto/update-quiz.input';
 
 @Resolver(() => Quiz)
 export class QuizzesResolver {
@@ -33,13 +32,4 @@ export class QuizzesResolver {
     return this.quizzesService.findNext(id);
   }
 
-  @Mutation(() => Quiz)
-  updateQuiz(@Args('updateQuizInput') updateQuizInput: UpdateQuizInput) {
-    return this.quizzesService.update(updateQuizInput.id, updateQuizInput);
-  }
-
-  @Mutation(() => Quiz)
-  removeQuiz(@Args('id', { type: () => Int }) id: number) {
-    return this.quizzesService.remove(id);
-  }
 }
